85e3afe4dfc294bb0505631e14b03746131fbb2f
Leonel <leonelmandarino@gmail.com>
2021-07-04 01:44:15 -0300

    Add workaround for heroku postgresql env var name

--- db.py
+++ db.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", os.getenv("LOCAL_DB_URL"))
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
